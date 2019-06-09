ab107d1e4524de49795efd45a4ab888404e60285
Librisius <fugaon@mail.ru>
2019-06-09 16:43:33 +0300

    template

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp 					= require('gulp'),
+	inlineCss 				= require('gulp-inline-css');
+
+
+
+
+gulp.task('watch', function(done) {
+
+	gulp.watch( 'code/**/*.html', html );
+});
+
+
+
+
+gulp.task('default', gulp.series('watch'));
+
+
+
+
+function html() {
+
+	gulp.src('code/**/*.html')
+
+		.pipe(
+			inlineCss({
+				applyWidthAttributes: true,
+				applyTableAttributes: true,
+				removeHtmlSelectors: true
+			})
+		)
+
+		.pipe(
+			gulp.dest('code/result')
+		);
+}
